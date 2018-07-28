ecfe63f24bccf2adb71be71a8698050b1c6b9267
Bophelo <mpilo@vivaldi.net>
2018-07-28 23:14:27 +0200

    Installing Twig

--- index.php
+++ index.php
@@ -13,9 +13,22 @@ $app = new \Slim\App([
 //not reinstantiated multiple times
 $container  = $app->getContainer();
 
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . 'resources/views', [
+        'cache' => 'false'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
+    return $view;
+};
+
 //routes
 $app->get('/', function () {
-    echo $this->nothing;
+    echo $this->view;
 });
 
 $app->run();
