a6dd18b330a454d1f72fbdc16fba791d252545fd
Bophelo <mpilo@vivaldi.net>
2018-07-28 22:58:58 +0200

    The Container

--- index.php
+++ index.php
@@ -5,13 +5,20 @@ require 'vendor/autoload.php';
 //app instance
 $app = new \Slim\App;
 
+//container e.g add database support, caching, other classes(dependency injection)...Singleton Pattern
+//not reinstantiated multiple times
+$container  = $app->getContainer();
+
+$container['greeting'] = function () {
+    echo 'abc';
+    return 'Hello from the Container';
+};
+
 //routes
 $app->get('/', function () {
-    echo 'Home';
-});
-
-$app->get('/users', function () {
-    echo 'Users';
+    echo $this->greeting;
+    echo $this->greeting;
+    echo $this->greeting;
 });
 
 $app->run();
