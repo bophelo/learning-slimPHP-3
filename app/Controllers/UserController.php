d80523f31ed03b3b95f2e5c9522021c196786a08
Bophelo <mpilo@vivaldi.net>
2018-08-01 00:14:50 +0200

    Helpful Model Methods

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\User;
+use PDO;
 
 class UserController extends BaseController
 {
-    public function index()
+    public function index($request, $response)
     {
-        return 'List of Users';
+        $users = $this->interface->db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_CLASS, User::class);
+
+        return $this->interface->view->render($response, 'users/index.twig', compact('users'));
     }
 
     public function show()
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,5 +4,19 @@ namespace App\Models;
 
 class User
 {
-    
+    public function fullName()
+    {
+        if ($this->last_name === null) {
+            return $this->first_name;
+        }
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    public function getFormattedBalance()
+    {
+        if ($this->balance == 0) {
+            return 'Zero Funds.';
+        }
+        return '£' . number_format($this->balance, 2);
+    }
 }
